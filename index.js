c842692cb3e9bf910e07c2e162b4e8598e841337
Paula <paula.batoon@gmail.com@users.noreply.github.com>
2020-12-11 21:30:50 -0500

    added modifier support

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ program
     .description('A command line interface to roll dice')
     .option('-d, --dice <dice>', 'dice command')
     .option('-a, --ability-roll', 'A standard 4d6 ability roll, dropping the lowest score')
+    .option('-c, --character <race>', 'Rolls a character with the specified racial modifiers')
     .option('--advantage', 'Roll with advantage')
     .option('--disadvantage', 'Roll with disadvantage')
     .parse(process.argv)
@@ -18,8 +19,34 @@ program
 
 const main = () => {
     //console.log(program.opts());
-    let output = 0;
+    let output = '';
     const labelColors = chalk.cyan;
+
+    // Verify -d input
+    if(program.dice && !rolls.verifyDiceInput(program.dice)) {
+        console.log(chalk.bold.red("Invalid format. Please enter dice option as <number of dice>d<number of dice sides> or d<number of dice sides>, i.e. 4d6 or d20"));
+        process.exit(1);
+    } 
+
+    // Base dice roll
+    if(program.dice) {
+        
+        let dice = rolls.diceRoll(program.dice);
+        output = dice;
+
+        // Add advantage/disadvantage if those options are selected
+        if(program.advantage) {
+            console.log(chalk.blue("Rolling with advantage."));
+            console.log(`Original roll: ${dice}`);
+            let advantageRoll = rolls.advantageRoll(dice);
+            output = chalk.green.bold(`Advantage roll: ${advantageRoll}`);
+        } else if (program.disadvantage) {
+            console.log(chalk.blue("Rolling with disadvantage."));
+            console.log(`Original roll: ${dice}`);
+            let disadvantageRoll = rolls.disadvantageRoll(dice);
+            output = chalk.green.bold(`Disadvantage roll: ${disadvantageRoll}`);
+        }
+    }
 
     // roll an ability roll if the user so chooses
     if(program.abilityRoll) {
@@ -33,30 +60,8 @@ const main = () => {
         process.exit(0);
     }
 
-    // Verify -d input
-    if(program.dice && !rolls.verifyDiceInput(program.dice)) {
-        console.log(chalk.bold.red("Invalid format. Please enter dice option as <number of dice>d<number of dice sides> or d<number of dice sides>, i.e. 4d6 or d20"));
-        process.exit(1);
-    } 
-
-    // Base dice roll
-    let dice = rolls.diceRoll(program.dice);
-    output = dice;
-
-    // Add advantage/disadvantage if those options are selected
-    if(program.advantage) {
-        console.log(chalk.blue("Rolling with advantage."));
-        console.log(`Original roll: ${dice}`);
-        let advantageRoll = rolls.advantageRoll(dice);
-        output = chalk.green.bold(`Advantage roll: ${advantageRoll}`);
-    } else if (program.disadvantage) {
-        console.log(chalk.blue("Rolling with disadvantage."));
-        console.log(`Original roll: ${dice}`);
-        let disadvantageRoll = rolls.disadvantageRoll(dice);
-        output = chalk.green.bold(`Disadvantage roll: ${disadvantageRoll}`);
-    }
-
     console.log(output);
+    process.exit(0);
 
 }
 
--- lib.js
+++ lib.js
@@ -1,6 +1,21 @@
+const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require('constants');
+const https = require('https');
+const chalk = require('chalk');
+const readline = require('readline');
+const { parse } = require('path');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+/*
+function Character() {
+}
+*/
+
 const verifyDiceInput = (input) => {
     // regex expressions for correct dice format
-    let format = "([1-9]\d|^)*d(?:4|6|8|10|12|20)";
+    let format = "(\d|^)*d(?:4|6|8|10|12|20|100)(\\+\\d+|$)";
 
     // Check if dice input is in the correct format
     if(input.match(format) == null) {
@@ -47,22 +62,45 @@ const abilityRoll = () => {
 
 const diceRoll = (input) => {
     //parse input into number of dice and dice sides
+    const inputString = String(input);
     const numDice = input.substr(0, input.indexOf("d"));
-    const diceSides = input.substr((input.indexOf("d") + 1));
-    
+    let diceSides = input.substr((input.indexOf("d") + 1));
+
     // If no prefix, then roll as single die
     if(input.indexOf("d") == 0) {
-        let roll = Math.floor(Math.random() * diceSides) + 1
+        let roll = Math.floor(Math.random() * diceSides) + 1;
         return roll;
     }
 
     let i = 0;
     let output = "";
 
+    // if input includes modifier, add all rolls together and add modifier
+    if(inputString.includes('+')){
+        let total = 0;
+        let modifier = inputString.split('+')[1];
+        let sidesString = inputString.split('d').pop().split('+');
+        diceSides = parseInt(sidesString);
+
+        while (i < numDice) {
+            let roll = Math.floor(Math.random() * diceSides) + 1;
+            total += roll;
+            //console.log(roll);
+            i++;
+        }
+
+        total += parseInt(modifier);
+        //console.log(modifier);
+        return total;
+        
+
+    }
+
     // for each number of dice specified, roll and append to output string
     while (i < numDice) {
         let roll = Math.floor(Math.random() * diceSides) + 1
         let str = roll + "\n"
+
         output += str;
         i++;
     }
@@ -71,10 +109,11 @@ const diceRoll = (input) => {
     
 }
 
+
 const advantageRoll = (input) => {
     let d20 = Math.floor(Math.random() * 20) + 1;
     let output = Math.max(input, d20);
-    
+
     return output;
 
 }
